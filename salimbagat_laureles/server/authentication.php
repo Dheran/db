429f7f97c65b256a01e68b41555e9a4afc44ae69
mgador <47278183+mgador@users.noreply.github.com>
2023-05-10 21:31:45 +0800

    Update authentication.php
    eto pri try mo

--- salimbagat_laureles/server/authentication.php
+++ salimbagat_laureles/server/authentication.php
@@ -1,35 +1,22 @@
 <?php
     session_start();
-    include ('connection.php');
+    require_once('connection.php');
     $username = $_POST ['user'];
     $password = $_POST ['pass'];
 
-    $username = stripcslashes($username);
-    $password = stripcslashes($password);
+    $sql = "SELECT * FROM tb_admin WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($conn, $ql);
 
-    $username = mysqli_real_escape_string($con, $username);
-    $password = mysqli_real_escape_string($con, $password);
-
-    // Check if the user is an admin
-    $admin_sql = "SELECT * FROM tb_admin WHERE username = '$admin_username' AND password = '$admin_password'";
-    $admin_result = mysqli_query($conn, $admin_sql);
-
-    if ($admin_result && mysqli_num_rows($admin_result) > 0) {
-        // Set the session variables for the admin user
-        $_SESSION['admin_username'] = $username;
-        header("Location: admin_homepage.php");
-    } else {
-        // Check if the user is a regular user
-        $user_sql = "SELECT * FROM tb_reg WHERE username = '$username' AND password = '$password'";
-        $user_result = mysqli_query($conn, $user_sql);
-
-        if ($user_result && mysqli_num_rows($user_result) > 0) {
-            // Set the session variables for the regular user
-            $row = mysqli_fetch_assoc($user_result);
-            $_SESSION['username'] = $row['username'];
-            header("Location: homepage.php");
+    if (mysqli_num_rows($result) > 0) {
+        $row = $result->fetch_assoc();
+        // Palitan mo rin sa admin_homepage mo tsaka sa homepage ung $_SESSION gawin mong username lang
+        $_SESSION['username'] = $username;
+        if ($row['user_type'] == 'admin') {
+            header("Location: admin_homepage.php");
         } else {
-            echo '<script>alert("Username and password are incorrect.");</script>';
+            header("Location: homepage.php");
         }
+    } else {  
+        echo '<script>alert("Username and password are incorrect.");</script>';
     }
 ?>
